f7113fa3ce06e89253d7f8e2466790cba616a419
AnhTuanDev <tuan.ho.designer@gmail.com>
2024-03-22 15:59:55 +0700

    Thứ sáu, 22 Tháng 3 năm 2024 15:59:55 +07 -- them trang bao loi 404...

---
+++ resources/views/errors/402.blade.php
@@ -0,0 +1,21 @@
+{{-- Page: errors.402 --}}
+
+<x-layouts.app>
+
+    <x-slot name="pageTitle">Trang 404</x-slot>
+
+    <section class="content">
+
+        <div class="content-body">
+            
+            <div class="container mx-auto min-h-50 errors-container">
+                
+                <h2>Không thể truy cập địa chỉ <b>{{ request()->path() }}</b> !</h2>
+                
+            </div>
+            
+        </div>
+
+    </section>
+
+</x-layouts.app>
